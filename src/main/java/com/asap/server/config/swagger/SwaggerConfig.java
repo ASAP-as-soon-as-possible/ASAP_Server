b9b11639f5aab4c038b2892ee6a56f65dfc7ca55
KWY <wodrk1001@gmail.com>
2023-06-29 23:29:37 +0900

    #7 [feat] swagger config 추가

---
+++ src/main/java/com/asap/server/config/swagger/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.asap.server.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("ASAP Swagger")
+                .description("ASAP API Docs")
+                .version("1.0.0");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
