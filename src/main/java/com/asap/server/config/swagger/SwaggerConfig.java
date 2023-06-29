86639af327f8ea4a1ed347ff5fa01e10e533de27
KWY <wodrk1001@gmail.com>
2023-06-30 02:06:58 +0900

    #7 [feat] swagger 내 cors 에러 수정

--- src/main/java/com/asap/server/config/swagger/SwaggerConfig.java
+++ src/main/java/com/asap/server/config/swagger/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.asap.server.config.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,7 @@ public class SwaggerConfig {
                 .version("1.0.0");
 
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .components(new Components())
                 .info(info);
     }
