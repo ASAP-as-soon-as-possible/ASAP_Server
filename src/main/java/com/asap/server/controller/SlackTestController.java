55612734a112251afaf97ae80da5efe579552157
sohyundoh <lyny.doh@gmail.com>
2023-07-01 18:43:11 +0900

    #4 [feat] slack 테스트 Api controller 작성

---
+++ src/main/java/com/asap/server/controller/SlackTestController.java
@@ -0,0 +1,18 @@
+package com.asap.server.controller;
+
+import com.asap.server.common.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class SlackTestController {
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse test(){
+        throw new IllegalArgumentException();
+    }
+}
