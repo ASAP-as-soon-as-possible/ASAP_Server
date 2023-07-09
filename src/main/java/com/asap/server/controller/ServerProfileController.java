1e0ebcdbba06c178f9cf01a143aec6a44fab3605
KWY <wodrk1001@gmail.com>
2023-07-09 21:18:21 +0900

    #19 [fix] profile 라우터 롤백

--- src/main/java/com/asap/server/controller/ServerProfileController.java
+++ src/main/java/com/asap/server/controller/ServerProfileController.java
@@ -1,12 +1,10 @@
 package com.asap.server.controller;
 
-import com.asap.server.config.resolver.meeting.MeetingId;
 import io.swagger.v3.oas.annotations.Hidden;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,12 +14,8 @@ public class ServerProfileController {
 
     private final Environment env;
 
-    @GetMapping("/profile/{meetingId}")
-    public String getProfile(
-            @PathVariable("meetingId") String meetingUrl,
-            @MeetingId Long meetingId
-    ) {
-        System.out.println("id is " + meetingId.toString());
+    @GetMapping("/profile")
+    public String getProfile() {
         return Arrays.stream(env.getActiveProfiles())
                 .findFirst()
                 .orElse("");
