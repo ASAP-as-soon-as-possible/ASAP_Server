f7a5b1c8b182d270595f6078edcd10bd4d12106f
sohyundoh <lyny.doh@gmail.com>
2023-07-09 21:28:51 +0900

    #21 [feat] 헤더 userId annotation 처리 로직 작성

---
+++ src/main/java/com/asap/server/config/resolver/user/UserIdResolver.java
@@ -0,0 +1,43 @@
+package com.asap.server.config.resolver.user;
+
+import com.asap.server.config.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotNull;
+
+@RequiredArgsConstructor
+@Component
+public class UserIdResolver implements HandlerMethodArgumentResolver {
+    private final JwtService jwtService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter){
+        return parameter.hasParameterAnnotation(UserId.class) && Long.class.equals(parameter.getParameterType());
+    }
+    @Override
+    public Object resolveArgument(@NotNull MethodParameter parameter, ModelAndViewContainer modelAndViewContainer, @NotNull NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        final HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        final String token = request.getHeader("Authorization").split(" ")[1];
+
+        // 토큰 검증
+        if (!jwtService.verifyToken(token)) {
+            throw new RuntimeException(String.format("USER_ID를 가져오지 못했습니다. (%s - %s)", parameter.getClass(), parameter.getMethod()));
+        }
+
+        // 유저 아이디 반환
+        final String tokenContents = jwtService.getJwtContents(token);
+        try {
+            return Long.parseLong(tokenContents);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(String.format("USER_ID를 가져오지 못했습니다. (%s - %s)", parameter.getClass(), parameter.getMethod()));
+        }
+    }
+
+}
