9883516e1a44a2a2d6aabf44434e43a93593f2c3
sohyundoh <lyny.doh@gmail.com>
2023-07-09 21:33:44 +0900

    #21 [fix] git 충돌 해결

--- src/main/java/com/asap/server/exception/Error.java
+++ src/main/java/com/asap/server/exception/Error.java
@@ -12,25 +12,16 @@ public enum Error {
     /**
     400 BAD REQUEST
      **/
-    INVALID_MULTIPART_EXTENSION_EXCEPTION(HttpStatus.BAD_REQUEST, "지원하지 않는 파일입니다"),
-<<<<<<< Updated upstream
     INVALID_MEETING_URL_EXCEPTION(HttpStatus.BAD_REQUEST, "유효하지 않는 URL 입니다."),
 
-=======
-    /*
-    * 401 UnauthorizedException
-     */
-    
->>>>>>> Stashed changes
+    /**
     /**
      401 UNAUTHORIZED
      **/
-<<<<<<< Updated upstream
     TOKEN_TIME_EXPIRED_EXCEPTION(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다."),
-=======
     NOT_FOUND_IMAGE_EXCEPTION(HttpStatus.NOT_FOUND, "이미지가 존재하지 않습니다."),
     NOT_FOUND_SAVE_IMAGE_EXCEPTION(HttpStatus.NOT_FOUND, "이미지가 저장되지 않았습니다."),
->>>>>>> Stashed changes
+
 
     /**
      * 500 INTERNAL SERVER ERROR
