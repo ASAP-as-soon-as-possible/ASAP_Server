27dae1e62a0c30cbdaa15f3fa556cf7ef6c4be65
sohyundoh <lyny.doh@gmail.com>
2023-07-11 15:45:39 +0900

    #28 [fix] enum에 String field 추가

--- src/main/java/com/asap/server/domain/enums/Place.java
+++ src/main/java/com/asap/server/domain/enums/Place.java
@@ -1,5 +1,16 @@
 package com.asap.server.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
 public enum Place {
-    ONLINE, OFFLINE, UNDEFINED
+    ONLINE("ONLINE"),
+    OFFLINE("OFFLINE"),
+    UNDEFINED("UNDEFINED");
+
+    @Getter
+    @JsonValue
+    private String place;
 }
