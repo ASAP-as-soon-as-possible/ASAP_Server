28faf242fa6cae0dab99fd8a5ef272829ca1353d
sohyundoh <lyny.doh@gmail.com>
2023-07-12 07:42:59 +0900

    #31 [feat] Request Body 작성

--- src/main/java/com/asap/server/controller/dto/request/MeetingTimeSaveRequestDto.java
+++ src/main/java/com/asap/server/controller/dto/request/MeetingTimeSaveRequestDto.java
@@ -3,17 +3,31 @@ package com.asap.server.controller.dto.request;
 import com.asap.server.domain.enums.TimeSlot;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.*;
 
 @Getter
 @AllArgsConstructor
 public class MeetingTimeSaveRequestDto {
 
+    @NotNull
     private DateDto date;
+
+    @NotNull
     private List<ScheduleDto> schedule;
     private class DateDto{
+
+        @Pattern(regexp = "\\d{2}", message = "날짜 형식이 맞지 않습니다.")
         private String month;
+
+
+        @Pattern(regexp = "\\d{2}", message = "날짜 형식이 맞지 않습니다.")
         private String day;
+
+
+        @Pattern(regexp = "[\\uAC00-\\uD7A3]", message = "날짜 형식이 맞지 않습니다.")
         private String dayOfWeek;
     }
     private class ScheduleDto{
--- src/main/java/com/asap/server/domain/DateAvailability.java
+++ src/main/java/com/asap/server/domain/DateAvailability.java
@@ -38,8 +38,28 @@ public class DateAvailability extends AuditingTimeEntity {
     public static DateAvailability newInstance(String date){
         String month = date.substring(5,7);
         String day = date.substring(8,10);
-        String dayOfWeek = date.substring(11,14);
+        String dayOfWeek = dayConverter(date.substring(11,14));
+
         return new DateAvailability(month, day, dayOfWeek);
     }
-
+    public static String dayConverter(String s){
+        switch (s) {
+            case "MON":
+                return "월";
+            case "TUE":
+                return "화";
+            case "WED":
+                return "수";
+            case "THU":
+                return "목";
+            case "FRI":
+                return "금";
+            case "SAT":
+                return "토";
+            case "SUN":
+                return "일";
+            default:
+                return "잘못된 값";
+        }
+    }
 }
