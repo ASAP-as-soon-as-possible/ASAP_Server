5770ed6c50b7fe539f59639380742d8e161229ef
sohyundoh <lyny.doh@gmail.com>
2023-07-12 14:38:48 +0900

    #31 [fix] Response Dto 생성자 수정

--- src/main/java/com/asap/server/controller/dto/response/MeetingSaveResponseDto.java
+++ src/main/java/com/asap/server/controller/dto/response/MeetingSaveResponseDto.java
@@ -1,10 +1,12 @@
 package com.asap.server.controller.dto.response;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MeetingSaveResponseDto {
     private String url;
     private String accessToken;
