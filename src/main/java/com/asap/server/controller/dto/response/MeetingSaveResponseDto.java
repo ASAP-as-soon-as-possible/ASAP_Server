30640b94f73af37807aaf32c3555e0c89fa79191
sohyundoh <lyny.doh@gmail.com>
2023-07-12 14:38:48 +0900

    Revert "#31 [fix] Response Dto 생성자 수정"
    This reverts commit 5770ed6c50b7fe539f59639380742d8e161229ef.

--- src/main/java/com/asap/server/controller/dto/response/MeetingSaveResponseDto.java
+++ src/main/java/com/asap/server/controller/dto/response/MeetingSaveResponseDto.java
@@ -1,12 +1,10 @@
 package com.asap.server.controller.dto.response;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class MeetingSaveResponseDto {
     private String url;
     private String accessToken;
