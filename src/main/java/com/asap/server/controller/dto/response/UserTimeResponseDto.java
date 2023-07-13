c882948702beeb68c514a9f3c114e58fd31ae158
sohyundoh <lyny.doh@gmail.com>
2023-07-14 08:55:38 +0900

    #45 [feat] Dto 작성

---
+++ src/main/java/com/asap/server/controller/dto/response/UserTimeResponseDto.java
@@ -0,0 +1,12 @@
+package com.asap.server.controller.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UserTimeResponseDto {
+    private String role;
+}
