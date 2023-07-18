338902ade2bc47aca41524d475eac14019a2f5c9
sohyundoh <lyny.doh@gmail.com>
2023-07-18 16:59:09 +0900

    #60 [feat] webconfig 변경사항 제거

--- src/main/java/com/asap/server/config/WebConfig.java
+++ src/main/java/com/asap/server/config/WebConfig.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
