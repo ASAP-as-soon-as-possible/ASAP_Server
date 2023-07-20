ad926a2ec8b1cc2bf08e1fbc9ff1e5a0b6f597ad
sohyundoh <lyny.doh@gmail.com>
2023-07-20 23:41:52 +0900

    #76 [chore] swagger 수정

--- src/main/java/com/asap/server/controller/UserController.java
+++ src/main/java/com/asap/server/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.asap.server.controller;
 
+import com.asap.server.common.dto.ErrorResponse;
 import com.asap.server.common.dto.SuccessResponse;
 import com.asap.server.config.resolver.meeting.MeetingId;
 import com.asap.server.config.resolver.user.UserId;
@@ -10,6 +11,10 @@ import com.asap.server.exception.Success;
 import com.asap.server.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -33,6 +38,11 @@ public class UserController {
 
     @Operation(summary = "[회의 가능 시간 입력 뷰 - 방장] 방장 가능 시간 입력 API")
     @SecurityRequirement(name = "JWT Auth")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유효한 회의 입니다."),
+            @ApiResponse(responseCode = "404", description = "해당 회의는 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/host/{meetingId}/time")
     public SuccessResponse createHostTime(
             @PathVariable("meetingId") String _meetingId,
@@ -44,6 +54,11 @@ public class UserController {
     }
 
     @Operation(summary = "[회의 가능 시간 입력 뷰 - 참여자] 참여자 정보 및 가능 시간 입력 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회의 선택지가 성공적으로 조회되었습니다."),
+            @ApiResponse(responseCode = "401", description = "해당 유저는 해당 방의 방장이 아닙니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "해당 회의는 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/{meetingId}/time")
     public SuccessResponse createMemberTime(
             @PathVariable("meetingId") String _meetingId,
@@ -54,6 +69,11 @@ public class UserController {
     }
 
     @Operation(summary = "[방장 입장 뷰] 방장 로그인 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회의 선택지가 성공적으로 조회되었습니다."),
+            @ApiResponse(responseCode = "401", description = "유효하지 않은 사용자 이름 또는 비밀번호입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "해당 회의는 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("{meetingId}/host")
     public SuccessResponse loginByHost(
             @PathVariable("meetingId") String _meetingId,
