98569708052da1830b6480d60a1d41e3f7234067
KWY <wodrk1001@gmail.com>
2023-08-08 00:10:27 +0900

    #111 [fix] 큐카드 api 수정

--- src/main/java/com/asap/server/service/MeetingService.java
+++ src/main/java/com/asap/server/service/MeetingService.java
@@ -14,6 +14,7 @@ import com.asap.server.controller.dto.response.MeetingSaveResponseDto;
 import com.asap.server.controller.dto.response.MeetingScheduleResponseDto;
 import com.asap.server.controller.dto.response.PreferTimeResponseDto;
 import com.asap.server.controller.dto.response.TimeTableResponseDto;
+import com.asap.server.domain.ConfirmedDateTime;
 import com.asap.server.domain.Meeting;
 import com.asap.server.domain.PreferTime;
 import com.asap.server.domain.User;
@@ -30,15 +31,15 @@ import com.asap.server.repository.PreferTimeRepository;
 import com.asap.server.service.vo.MeetingTimeVo;
 import com.asap.server.service.vo.MeetingVo;
 import com.asap.server.service.vo.UserVo;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Base64Utils;
-
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Base64Utils;
 
 import static com.asap.server.exception.Error.INVALID_MEETING_HOST_EXCEPTION;
 
@@ -165,16 +166,23 @@ public class MeetingService {
                 .map(User::getName)
                 .collect(Collectors.toList());
 
+        ConfirmedDateTime confirmedDateTime = meeting.getConfirmedDateTime();
+        String month = String.valueOf(confirmedDateTime.getStartDateTime().getMonthValue());
+        String day = String.valueOf(confirmedDateTime.getStartDateTime().getDayOfMonth());
+        String dayOfWeek = confirmedDateTime.getStartDateTime().getDayOfWeek().toString();
+        String startTime = confirmedDateTime.getStartDateTime().toLocalTime().toString();
+        String endTime = confirmedDateTime.getEndDateTime().toLocalTime().toString();
+
         return FixedMeetingResponseDto
                 .builder()
                 .title(meeting.getTitle())
                 .place(meeting.getPlace().toString())
-                .placeDetail(meeting.getPlaceDetail())
-                .month(Integer.valueOf(meeting.getMonth()).toString())
-                .day(Integer.valueOf(meeting.getDay()).toString())
-                .dayOfWeek(meeting.getDayOfWeek())
-                .startTime(meeting.getStartTime().getTime())
-                .endTime(meeting.getEndTime().getTime())
+                .placeDetail(meeting.getPlace().getPlaceDetail())
+                .month(month)
+                .day(day)
+                .dayOfWeek(dayOfWeek)
+                .startTime(startTime)
+                .endTime(endTime)
                 .hostName(meeting.getHost().getName())
                 .userNames(userNames)
                 .additionalInfo(meeting.getAdditionalInfo())
