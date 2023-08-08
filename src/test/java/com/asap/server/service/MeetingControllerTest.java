9248ea931bb4637d46cc450f042ead4614761607
sohyundoh <lyny.doh@gmail.com>
2023-08-08 11:18:38 +0900

    #110 [fix] 회의 생성 test

---
+++ src/test/java/com/asap/server/service/MeetingControllerTest.java
@@ -0,0 +1,74 @@
+package com.asap.server.service;
+
+import com.asap.server.controller.MeetingController;
+import com.asap.server.controller.dto.request.MeetingSaveRequestDto;
+import com.asap.server.controller.dto.request.PreferTimeSaveRequestDto;
+import com.asap.server.domain.enums.Duration;
+import com.asap.server.domain.enums.PlaceType;
+import com.asap.server.domain.enums.TimeSlot;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MeetingControllerTest {
+
+    @InjectMocks
+    private MeetingController meetingController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    public void beforeEach() {
+        mockMvc = MockMvcBuilders.standaloneSetup(meetingController).build();
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("회의가 저장된다.")
+    void createMeeting() throws Exception {
+        List<String> date = new ArrayList<>();
+        date.add("2023/08/07/MON");
+        date.add("2023/08/08/TUE");
+        List<PreferTimeSaveRequestDto> preferTimeDtos = new ArrayList<>();
+        preferTimeDtos.add(new PreferTimeSaveRequestDto(TimeSlot.SLOT_6_00, TimeSlot.SLOT_12_00));
+        MeetingSaveRequestDto saveRequestDto = new MeetingSaveRequestDto(
+                "테스트 회의",
+                date,
+                preferTimeDtos,
+                PlaceType.OFFLINE,
+                "어디서 만날까",
+                Duration.HOUR_HALF,
+                "서지원",
+                "2028",
+                "추가 정보 비밀"
+        );
+        mockMvc.perform(
+                post("/meeting")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(saveRequestDto))
+        ).andExpect(status().isOk());
+
+    }
+}
