e79e2bd1d7a7fb6c50ead7e3913222651e30b8ec
sohyundoh <lyny.doh@gmail.com>
2023-08-09 17:24:44 +0900

    #122 [fix] static of 함수 적용

--- src/main/java/com/asap/server/controller/dto/response/ScheduleDateResponseDto.java
+++ src/main/java/com/asap/server/controller/dto/response/ScheduleDateResponseDto.java
@@ -1,5 +1,7 @@
 package com.asap.server.controller.dto.response;
 
+import com.asap.server.common.utils.DayOfWeekConverter;
+import com.asap.server.domain.AvailableDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,4 +13,12 @@ public class ScheduleDateResponseDto {
     private String month;
     private String day;
     private String dayOfWeek;
+
+    public static ScheduleDateResponseDto of(AvailableDate availableDate){
+        return new ScheduleDateResponseDto(
+                String.valueOf(availableDate.getDate().getMonthValue()),
+                String.valueOf(availableDate.getDate().getDayOfMonth()),
+                DayOfWeekConverter.convertDayOfWeekEnToKo(availableDate.getDate().getDayOfWeek())
+        );
+    }
 }
