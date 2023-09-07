3a489c8e893c4004562ed28cbe1268355dd32ab8
KWY <wodrk1001@gmail.com>
2023-09-07 20:07:42 +0900

    #112 [feat] 비즈니스 로직 구현

--- src/main/java/com/asap/server/service/UserV2Service.java
+++ src/main/java/com/asap/server/service/UserV2Service.java
@@ -1,14 +1,18 @@
 package com.asap.server.service;
 
+import com.asap.server.controller.dto.request.UserRequestDto;
 import com.asap.server.domain.MeetingV2;
 import com.asap.server.domain.UserV2;
 import com.asap.server.domain.enums.Role;
+import com.asap.server.exception.model.NotFoundException;
 import com.asap.server.repository.UserV2Repository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.asap.server.exception.Error.USER_NOT_FOUND_EXCEPTION;
 
 @Service
 @RequiredArgsConstructor
@@ -31,5 +35,14 @@ public class UserV2Service {
                 .stream()
                 .map(UserV2::getName)
                 .collect(Collectors.toList());
+    }
+
+    public void setFixedUsers(final List<UserRequestDto> users) {
+        users.forEach(user -> {
+            UserV2 fixedUser = userV2Repository
+                    .findById(user.getId())
+                    .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND_EXCEPTION));
+            fixedUser.setIsFixed(true);
+        });
     }
 }
