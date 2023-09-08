533dd76cb33c00ce1fcb233f5b583d153e0d07bb
sohyundoh <lyny.doh@gmail.com>
2023-09-08 16:33:53 +0900

    #115 [fix] * 변경

--- src/main/java/com/asap/server/domain/TimeBlockUser.java
+++ src/main/java/com/asap/server/domain/TimeBlockUser.java
@@ -1,6 +1,10 @@
 package com.asap.server.domain;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
