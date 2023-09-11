4902cfa8b858ead3baabf3c5298b6ff3e723ca4f
KWY <wodrk1001@gmail.com>
2023-09-11 17:05:15 +0900

    #113 [fix] available date 서비스 수정

--- src/main/java/com/asap/server/service/AvailableDateService.java
+++ src/main/java/com/asap/server/service/AvailableDateService.java
@@ -7,6 +7,8 @@ import com.asap.server.domain.MeetingV2;
 import com.asap.server.exception.Error;
 import com.asap.server.exception.model.NotFoundException;
 import com.asap.server.repository.AvailableDateRepository;
+import com.asap.server.service.vo.TimeBlockVo;
+import com.asap.server.service.vo.TimeBlocksByDateVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,15 +21,14 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class AvailableDateService {
     private final AvailableDateRepository availableDateRepository;
+    private final TimeBlockService timeBlockService;
 
     private LocalDate dateFormatter(final String stringOfDate) {
         return LocalDate.parse(stringOfDate, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
     }
 
     public List<AvailableDateResponseDto> getAvailableDates(final MeetingV2 meetingV2) {
-        List<AvailableDate> availableDates = availableDateRepository.findByMeeting(meetingV2);
-
-        if (availableDates.isEmpty()) throw new NotFoundException(Error.AVAILABLE_DATE_NOT_FOUND_EXCEPTION);
+        List<AvailableDate> availableDates = findAvailableDates(meetingV2);
 
         return availableDates.stream()
                 .map(availableDate ->
@@ -37,6 +38,28 @@ public class AvailableDateService {
                                 .dayOfWeek(DateUtil.getDayOfWeek(availableDate.getDate()))
                                 .build())
                 .collect(Collectors.toList());
+    }
+
+    public List<TimeBlocksByDateVo> getAvailableDateVos(final MeetingV2 meetingV2) {
+        List<AvailableDate> availableDates = findAvailableDates(meetingV2);
+
+        return availableDates.stream()
+                .map(availableDate -> {
+                    List<TimeBlockVo> timeBlocks = timeBlockService
+                            .getTimeBlocksByAvailableDate(availableDate)
+                            .stream()
+                            .map(TimeBlockVo::of)
+                            .collect(Collectors.toList());
+
+                    return TimeBlocksByDateVo.of(availableDate, timeBlocks);
+                }).collect(Collectors.toList());
+    }
+
+    private List<AvailableDate> findAvailableDates(final MeetingV2 meeting) {
+        List<AvailableDate> availableDates = availableDateRepository.findByMeeting(meeting);
+
+        if (availableDates.isEmpty()) throw new NotFoundException(Error.AVAILABLE_DATE_NOT_FOUND_EXCEPTION);
+        return availableDates;
     }
 
     public void create(final MeetingV2 meeting, final List<String> availableDates) {
