6cfddb9d8cb490a568a45669b247b10deba3ba2e
sohyundoh <lyny.doh@gmail.com>
2023-09-15 18:57:14 +0900

    #169 [fix] 코드 포맷팅

--- src/main/java/com/asap/server/controller/UserController.java
+++ src/main/java/com/asap/server/controller/UserController.java
@@ -65,11 +65,12 @@ public class UserController {
 
     @Operation(summary = "[회의 가능 시간 입력 뷰 - 참여자] 참여자 정보 및 가능 시간 입력 API")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "회의 선택지가 성공적으로 조회되었습니다."), @ApiResponse(responseCode = "400",
-            description = "1. 시간 형식이 잘못되었습니다. [YYYY/MM/DD HH:MM]\n"
-                    + "2. 중복 입력된 시간이 있습니다.\n"
-                    + "3. 입력한 시간이 회의 가능 일시에 해당하지 않습니다.",
-            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "200", description = "회의 선택지가 성공적으로 조회되었습니다."),
+            @ApiResponse(responseCode = "400",
+                    description = "1. 시간 형식이 잘못되었습니다. [YYYY/MM/DD HH:MM]\n"
+                            + "2. 중복 입력된 시간이 있습니다.\n"
+                            + "3. 입력한 시간이 회의 가능 일시에 해당하지 않습니다.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
             @ApiResponse(responseCode = "404", description = "해당 회의는 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PostMapping("/{meetingId}/time")
