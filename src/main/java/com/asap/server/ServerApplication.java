61a9c1ff4600e922392c49a3a4c25490fefbc279
KWY <wodrk1001@gmail.com>
2023-09-19 21:41:54 +0900

    #183 [feat] aop 를 통한 로그 기능 구현

--- src/main/java/com/asap/server/ServerApplication.java
+++ src/main/java/com/asap/server/ServerApplication.java
@@ -3,9 +3,11 @@ package com.asap.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class ServerApplication {
 
