f15c11c275a6cd4aee3d47e879a8690a350e073c
KWY <wodrk1001@gmail.com>
2023-10-01 20:25:44 +0900

    #208 [feat] delete jpql query 구현

--- src/main/java/com/asap/server/repository/PreferTimeRepository.java
+++ src/main/java/com/asap/server/repository/PreferTimeRepository.java
@@ -2,7 +2,10 @@ package com.asap.server.repository;
 
 import com.asap.server.domain.Meeting;
 import com.asap.server.domain.PreferTime;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +14,9 @@ public interface PreferTimeRepository extends Repository<PreferTime, Long> {
     PreferTime save(final PreferTime preferTime);
 
     List<PreferTime> findByMeeting(final Meeting meeting);
+
+
+    @Modifying
+    @Query("delete from PreferTime p where p.meeting = :meeting")
+    void deleteByMeeting(@Param("meeting") final Meeting meeting);
 }
--- src/main/java/com/asap/server/repository/TimeBlockUserRepository.java
+++ src/main/java/com/asap/server/repository/TimeBlockUserRepository.java
@@ -3,7 +3,10 @@ package com.asap.server.repository;
 import com.asap.server.domain.TimeBlock;
 import com.asap.server.domain.TimeBlockUser;
 import com.asap.server.domain.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +17,8 @@ public interface TimeBlockUserRepository extends Repository<TimeBlockUser, Long>
     List<TimeBlockUser> findAllByUser(final User user);
 
     List<TimeBlockUser> findByTimeBlock(final TimeBlock timeBlock);
+
+    @Modifying
+    @Query("delete from TimeBlockUser t where t.timeBlock in :timeblocks")
+    void deleteByTimeBlocksIn(@Param("timeblocks") final List<TimeBlock> timeBlocks);
 }
