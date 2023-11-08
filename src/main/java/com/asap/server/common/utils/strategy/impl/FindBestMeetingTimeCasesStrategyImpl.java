fc7ddc70fd62465fd0e387ce7541841d060f6a4f
KWY <wodrk1001@gmail.com>
2023-11-08 22:05:44 +0900

    [refactor] time case 알고리즘 함수화

--- src/main/java/com/asap/server/common/utils/strategy/impl/FindBestMeetingTimeCasesStrategyImpl.java
+++ src/main/java/com/asap/server/common/utils/strategy/impl/FindBestMeetingTimeCasesStrategyImpl.java
@@ -10,27 +10,48 @@ import java.util.List;
 
 @Component
 public class FindBestMeetingTimeCasesStrategyImpl implements FindBestMeetingTimeCasesStrategy {
+    private static final Duration[] durations = Duration.values();
+
     @Override
     public List<PossibleTimeCaseVo> find(final Duration duration, int userCount) {
-        Duration[] durations = Duration.values();
         List<PossibleTimeCaseVo> timeCases = new ArrayList<>();
         while (userCount > 0) {
-            timeCases.addAll(getPossibleMeetingTimeCases(durations, duration, userCount));
+            List<PossibleTimeCaseVo> possibleMeetingTimeCases = getPossibleMeetingTimeCases(duration, userCount);
+            timeCases.addAll(possibleMeetingTimeCases);
+
             userCount = userCount / 2;
         }
         return timeCases;
     }
 
-    private List<PossibleTimeCaseVo> getPossibleMeetingTimeCases(final Duration[] durations, final Duration duration, final int userCount) {
+    private List<PossibleTimeCaseVo> getPossibleMeetingTimeCases(final Duration duration, final int userCount) {
+        List<PossibleTimeCaseVo> firstTimeCases = findFirstPossibleMeetingTimeCases(duration, userCount);
+
+        if (duration.ordinal() < 2) return firstTimeCases;
+
+        List<PossibleTimeCaseVo> secondTimeCases = findSecondPossibleMeetingTimeCases(duration, userCount);
+        firstTimeCases.addAll(secondTimeCases);
+        return firstTimeCases;
+    }
+
+    private List<PossibleTimeCaseVo> findFirstPossibleMeetingTimeCases(final Duration duration, final int userCount) {
         List<PossibleTimeCaseVo> timeCases = new ArrayList<>();
+
         for (int count = userCount; count > userCount / 2; count--) {
             timeCases.add(new PossibleTimeCaseVo(durations[duration.ordinal()], count));
-            if (duration.ordinal() > 0)
-                timeCases.add(new PossibleTimeCaseVo(durations[duration.ordinal() - 1], count));
+
+            if (duration.ordinal() < 1) continue;
+
+            timeCases.add(new PossibleTimeCaseVo(durations[duration.ordinal() - 1], count));
         }
 
-        int secondDuration = (duration.ordinal() >= 2) ? duration.ordinal() - 2 : -1;
+        return timeCases;
+    }
 
+    private List<PossibleTimeCaseVo> findSecondPossibleMeetingTimeCases(final Duration duration, final int userCount) {
+        List<PossibleTimeCaseVo> timeCases = new ArrayList<>();
+
+        int secondDuration = duration.ordinal() - 2;
         for (int durationCount = secondDuration; durationCount > -1; durationCount--) {
             for (int count = userCount; count > userCount / 2; count--) {
                 timeCases.add(new PossibleTimeCaseVo(durations[durationCount], count));
--- src/test/java/com/asap/server/common/utils/strategy/FindBestMeetingTimeCasesStrategyTest.java
+++ src/test/java/com/asap/server/common/utils/strategy/FindBestMeetingTimeCasesStrategyTest.java
@@ -63,4 +63,64 @@ class FindBestMeetingTimeCasesStrategyTest {
         assertThat(result.size()).isEqualTo(response.size());
         assertThat(result).isEqualTo(response);
     }
+
+    @DisplayName("회의 진행 시간이 30분이고, 회의 참여 인원이 1명일 때 1개의 경우의 수가 나온다.")
+    @Test
+    void findOptimalMeetingTimeCasesTest2() {
+        // given
+        Duration duration = HALF;
+        int userCount = 1;
+        List<PossibleTimeCaseVo> response = List.of(
+                new PossibleTimeCaseVo(HALF, 1)
+        );
+
+        // when
+        List<PossibleTimeCaseVo> result = strategy.find(duration, userCount);
+
+        // then
+        assertThat(result.size()).isEqualTo(response.size());
+        assertThat(result).isEqualTo(response);
+    }
+
+    @DisplayName("회의 진행 시간이 1시간이고, 회의 참여 인원이 1명일 때 2개의 경우의 수가 나온다.")
+    @Test
+    void findOptimalMeetingTimeCasesTest3() {
+        // given
+        Duration duration = HOUR;
+        int userCount = 1;
+        List<PossibleTimeCaseVo> response = List.of(
+                new PossibleTimeCaseVo(HOUR, 1),
+                new PossibleTimeCaseVo(HALF, 1)
+        );
+
+        // when
+        List<PossibleTimeCaseVo> result = strategy.find(duration, userCount);
+
+        // then
+        assertThat(result.size()).isEqualTo(response.size());
+        assertThat(result).isEqualTo(response);
+    }
+
+    @DisplayName("회의 진행 시간이 1시간 30분이고, 회의 참여 인원이 2명일 때 6개의 경우의 수가 나온다.")
+    @Test
+    void findOptimalMeetingTimeCasesTest4() {
+        // given
+        Duration duration = HOUR_HALF;
+        int userCount = 2;
+        List<PossibleTimeCaseVo> response = List.of(
+                new PossibleTimeCaseVo(HOUR_HALF, 2),
+                new PossibleTimeCaseVo(HOUR, 2),
+                new PossibleTimeCaseVo(HALF, 2),
+                new PossibleTimeCaseVo(HOUR_HALF, 1),
+                new PossibleTimeCaseVo(HOUR, 1),
+                new PossibleTimeCaseVo(HALF, 1)
+        );
+
+        // when
+        List<PossibleTimeCaseVo> result = strategy.find(duration, userCount);
+
+        // then
+        assertThat(result.size()).isEqualTo(response.size());
+        assertThat(result).isEqualTo(response);
+    }
 }
