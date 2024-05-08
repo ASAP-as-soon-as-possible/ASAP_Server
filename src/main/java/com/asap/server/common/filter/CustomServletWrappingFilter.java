4a10253b7bee827e96bd137e6aebc463b30e5855
KWY <wodrk1001@gmail.com>
2024-05-08 23:05:27 +0900

    #259 [del] filter 내 component 제거

--- src/main/java/com/asap/server/common/filter/CustomServletWrappingFilter.java
+++ src/main/java/com/asap/server/common/filter/CustomServletWrappingFilter.java
@@ -1,17 +1,15 @@
 package com.asap.server.common.filter;
 
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.ContentCachingRequestWrapper;
-import org.springframework.web.util.ContentCachingResponseWrapper;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
 import java.io.IOException;
 
-@Component
 public class CustomServletWrappingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(final HttpServletRequest request,
