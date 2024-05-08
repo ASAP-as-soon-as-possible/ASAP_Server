d810526bb18a09d1028458e013fad578ce99be97
KWY <wodrk1001@gmail.com>
2024-05-09 00:44:28 +0900

    #259 [feat] request wrapping 방식 변경

--- src/main/java/com/asap/server/common/filter/CustomServletWrappingFilter.java
+++ src/main/java/com/asap/server/common/filter/CustomServletWrappingFilter.java
@@ -1,25 +1,19 @@
 package com.asap.server.common.filter;
 
+import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.ContentCachingRequestWrapper;
-import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
 
-public class CustomServletWrappingFilter extends OncePerRequestFilter {
+public class CustomServletWrappingFilter implements Filter {
+
     @Override
-    protected void doFilterInternal(final HttpServletRequest request,
-                                    final HttpServletResponse response,
-                                    final FilterChain chain) throws ServletException, IOException {
-        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
-        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
-
-        chain.doFilter(requestWrapper, responseWrapper);
-
-        responseWrapper.copyBodyToResponse();
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = new CustomHttpServletRequestWrapper((HttpServletRequest) servletRequest);
+        filterChain.doFilter(request, servletResponse);
     }
 }
