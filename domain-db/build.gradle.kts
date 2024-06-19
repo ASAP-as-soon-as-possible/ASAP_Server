1ec6abba2d0c457680b9abcf21f190b6cf093dff
KWY <wodrk1001@gmail.com>
2024-06-19 15:30:06 +0900

    #262 [chore] dmain-rds 의존성 추가

--- domain-db/build.gradle.kts
+++ domain-db/build.gradle.kts
@@ -1,5 +1,30 @@
-dependencies {
+plugins {
+    kotlin("plugin.spring") version "1.9.23"
+    kotlin("plugin.jpa") version "1.9.23"
+    kotlin("kapt")
+}
 
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+val queryDslVersion = "5.0.0" // QueryDSL Version Setting
+
+dependencies {
+    // jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // QueryDSL Implementation
+    api("com.querydsl:querydsl-jpa:${queryDslVersion}:jakarta")
+    kapt("com.querydsl:querydsl-apt:${queryDslVersion}:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+    implementation(kotlin("stdlib-jdk8"))
+
+    // DB
+    runtimeOnly("com.mysql:mysql-connector-j")
 }
 
 kotlin {
