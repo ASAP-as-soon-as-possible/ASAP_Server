666e14ca0fad5813f8e2336b3f9699a9010d12a9
sohyundoh <lyny.doh@gmail.com>
2024-06-20 11:05:47 +0900

    #262 [feat] common 모듈 추가

---
+++ common/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
