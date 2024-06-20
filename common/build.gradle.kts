783551228379cd3c3e55aeb6e45bee993ba95aa2
sohyundoh <lyny.doh@gmail.com>
2024-06-20 12:06:13 +0900

    #262 [fix] gradle 의존성 수정

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,6 +1,3 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
 }
