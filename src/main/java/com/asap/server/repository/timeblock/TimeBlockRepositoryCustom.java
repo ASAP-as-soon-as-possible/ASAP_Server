795a0a8b34b998b6cad849314ccb2242111652d3
KWY <wodrk1001@gmail.com>
2024-07-03 23:38:23 +0900

    #271 [feat] 회의 내에 있는 시간 정보 조회 쿼리 수정
    이전 쿼리문은 available date를 기준으로 사용했기 때문에 최대 7번의 쿼리를
    실행했지만, 개선된 쿼리는 한번의 쿼리로 모든 정보를 가져올 수 있습니다

--- src/main/java/com/asap/server/repository/timeblock/TimeBlockRepositoryCustom.java
+++ src/main/java/com/asap/server/repository/timeblock/TimeBlockRepositoryCustom.java
@@ -1,9 +1,11 @@
 package com.asap.server.repository.timeblock;
 
+import com.asap.server.repository.timeblock.dto.TimeBlockDto;
 import com.asap.server.service.vo.TimeBlockVo;
 
 import java.util.List;
 
 public interface TimeBlockRepositoryCustom {
-    List<TimeBlockVo> findByAvailableDate(final Long availableId);
+
+    List<TimeBlockDto> findAllTimeBlockByMeeting(final Long meetingId);
 }
