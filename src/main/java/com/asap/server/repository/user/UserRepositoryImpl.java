84673076adc04d79b5b4bc5007d8fe0cdc7fcdf7
KWY <wodrk1001@gmail.com>
2024-07-04 00:04:19 +0900

    #271 [feat] 최적의 회의 시간에 참여하는 유저 조회 쿼리 수정
    available date의 id에서 date로 조회하는 것으로 수정하여
    inner join을 하나 더 추가했습니다.

--- src/main/java/com/asap/server/repository/user/UserRepositoryImpl.java
+++ src/main/java/com/asap/server/repository/user/UserRepositoryImpl.java
@@ -1,17 +1,18 @@
 package com.asap.server.repository.user;
 
+import static com.asap.server.domain.QAvailableDate.availableDate;
+import static com.asap.server.domain.QTimeBlock.timeBlock;
+import static com.asap.server.domain.QTimeBlockUser.timeBlockUser;
+import static com.asap.server.domain.QUser.user;
+
 import com.asap.server.domain.Meeting;
 import com.asap.server.domain.enums.TimeSlot;
 import com.asap.server.service.vo.QUserVo;
 import com.asap.server.service.vo.UserVo;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
-
+import java.time.LocalDate;
 import java.util.List;
-
-import static com.asap.server.domain.QTimeBlock.timeBlock;
-import static com.asap.server.domain.QTimeBlockUser.timeBlockUser;
-import static com.asap.server.domain.QUser.user;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom {
@@ -29,16 +30,17 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
     }
 
     @Override
-    public List<UserVo> findByAvailableDateAndTimeSlots(Long availableDateId, List<TimeSlot> timeSlots) {
-        return queryFactory.select(new QUserVo(
-                        user.id,
-                        user.name)
-                )
-                .from(timeBlockUser)
-                .innerJoin(timeBlock).on(timeBlockUser.timeBlock.id.eq(timeBlock.id)
-                        .and(timeBlock.availableDate.id.eq(availableDateId))
-                        .and(timeBlock.timeSlot.in(timeSlots)))
+    public List<UserVo> findByAvailableDateAndTimeSlots(LocalDate date, List<TimeSlot> timeSlots) {
+        return queryFactory.select(
+                        new QUserVo(
+                                user.id,
+                                user.name
+                        )
+                ).from(timeBlockUser)
+                .innerJoin(timeBlock).on(timeBlockUser.timeBlock.id.eq(timeBlock.id))
                 .innerJoin(user).on(timeBlockUser.user.id.eq(user.id))
+                .innerJoin(availableDate).on(timeBlock.availableDate.id.eq(availableDate.id))
+                .where(availableDate.date.eq(date).and(timeBlock.timeSlot.in(timeSlots)))
                 .groupBy(user.id)
                 .fetch();
     }
