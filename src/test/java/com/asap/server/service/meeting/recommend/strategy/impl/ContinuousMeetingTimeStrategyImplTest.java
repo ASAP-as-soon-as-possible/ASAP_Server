47e5e478b9238b559c032b2a9d005897a636cc6c
KWY <wodrk1001@gmail.com>
2024-07-31 21:35:49 +0900

    #291 [test] 30분 밖에 없는 상황 테스트 코드 작성

--- src/test/java/com/asap/server/service/meeting/recommend/strategy/impl/ContinuousMeetingTimeStrategyImplTest.java
+++ src/test/java/com/asap/server/service/meeting/recommend/strategy/impl/ContinuousMeetingTimeStrategyImplTest.java
@@ -1,6 +1,7 @@
 package com.asap.server.service.meeting.recommend.strategy.impl;
 
 import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_12_00;
+import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_12_30;
 import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_16_00;
 import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_18_00;
 import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_20_00;
@@ -189,6 +190,38 @@ class ContinuousMeetingTimeStrategyImplTest {
 
             // then
             assertThat(response).isEqualTo(result);
+        }
+    }
+
+    @DisplayName("timeBlocks 내에 12시부터 12시 30분까지 30분 간격으로 있고")
+    @Nested
+    class ReturnOneBestMeetingTimeVoByDuration2 {
+        // given
+        LocalDate availableDate = LocalDate.of(2023, 7, 10);
+        List<TimeBlockDto> timeBlocks = TimeBlockDtoGenerator.generator(availableDate, SLOT_12_00, SLOT_12_30, 0, 2L);
+
+        @DisplayName("회의 진행 시간이 30분일 때, 12시부터 12시 30분까지인 BestMeetingTimeVo를 반환한다.")
+        @Test
+        void durationHalfTest() {
+            // given
+            BestMeetingTimeVo r1 = new BestMeetingTimeVo(availableDate, SLOT_12_00, SLOT_12_30, 0);
+            List<BestMeetingTimeVo> result = List.of(r1);
+
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.HALF);
+
+            // then
+            assertThat(response).isEqualTo(result);
+        }
+
+        @DisplayName("회의 진행 시간이 1시간일 때, 빈 리스트를 반환한다.")
+        @Test
+        void durationHourTest() {
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.HOUR);
+
+            // then
+            assertThat(response.size()).isZero();
         }
     }
 }
