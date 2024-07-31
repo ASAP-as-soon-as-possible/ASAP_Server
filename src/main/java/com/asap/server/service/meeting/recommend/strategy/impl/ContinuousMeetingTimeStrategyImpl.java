4a3a6f1cb1d6dfe9c64d10095396e025bb962819
KWY <wodrk1001@gmail.com>
2024-07-31 23:55:51 +0900

    #291 [refactor] 일부 로직 함수로 추출
    총 회의 시간의 가중치의 합을 구하는 로직,
    회의 진행 시간을 만족하는지 구분하는 로직 추출

--- src/main/java/com/asap/server/service/meeting/recommend/strategy/impl/ContinuousMeetingTimeStrategyImpl.java
+++ src/main/java/com/asap/server/service/meeting/recommend/strategy/impl/ContinuousMeetingTimeStrategyImpl.java
@@ -1,11 +1,9 @@
 package com.asap.server.service.meeting.recommend.strategy.impl;
 
 import com.asap.server.persistence.domain.enums.Duration;
-import com.asap.server.persistence.domain.enums.TimeSlot;
 import com.asap.server.persistence.repository.timeblock.dto.TimeBlockDto;
 import com.asap.server.service.meeting.recommend.strategy.ContinuousMeetingTimeStrategy;
 import com.asap.server.service.vo.BestMeetingTimeVo;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Component;
@@ -14,51 +12,58 @@ import org.springframework.stereotype.Component;
 public class ContinuousMeetingTimeStrategyImpl implements ContinuousMeetingTimeStrategy {
     @Override
     public List<BestMeetingTimeVo> find(List<TimeBlockDto> timeBlocks, Duration duration) {
-        LocalDate date = timeBlocks.get(0).availableDate();
         int startIdx = 0;
         int endIdx = 1;
+
         List<BestMeetingTimeVo> response = new ArrayList<>();
         while (endIdx < timeBlocks.size()) {
+            TimeBlockDto endTimeBlock = timeBlocks.get(endIdx - 1);
             TimeBlockDto nextTimeBlock = timeBlocks.get(endIdx);
-            if (date.isEqual(nextTimeBlock.availableDate())
-                    && timeBlocks.get(endIdx - 1).timeSlot().ordinal() + 1 == nextTimeBlock.timeSlot().ordinal()) {
+            if (endTimeBlock.availableDate().isEqual(nextTimeBlock.availableDate())
+                    && endTimeBlock.timeSlot().ordinal() + 1 == nextTimeBlock.timeSlot().ordinal()) {
                 endIdx++;
                 continue;
             }
 
             TimeBlockDto startTimeBlock = timeBlocks.get(startIdx);
-            TimeBlockDto endTimeBlock = timeBlocks.get(endIdx - 1);
-            int blockCnt = endTimeBlock.timeSlot().ordinal() - startTimeBlock.timeSlot().ordinal();
-
-            if (blockCnt >= duration.getNeedBlock()) {
-                int weight = timeBlocks
-                        .subList(startIdx, endIdx - 1)
-                        .stream()
-                        .map(TimeBlockDto::weight)
-                        .reduce(0, Integer::sum);
-
-                response.add(
-                        new BestMeetingTimeVo(date, startTimeBlock.timeSlot(), endTimeBlock.timeSlot(), weight));
+            if (isSatisfiedDuration(startTimeBlock, endTimeBlock, duration)) {
+                int weight = sumTimeBlocksWeight(timeBlocks, startIdx, endIdx - 1);
+                response.add(new BestMeetingTimeVo(startTimeBlock.availableDate(), startTimeBlock.timeSlot(),
+                        endTimeBlock.timeSlot(), weight));
             }
             startIdx = endIdx++;
-            date = nextTimeBlock.availableDate();
-
         }
+
         if (startIdx < endIdx) {
             TimeBlockDto startTimeBlock = timeBlocks.get(startIdx);
             TimeBlockDto endTimeBlock = timeBlocks.get(endIdx - 1);
-            int blockCnt = endTimeBlock.timeSlot().ordinal() - startTimeBlock.timeSlot().ordinal();
-
-            if (blockCnt >= duration.getNeedBlock()) {
-                int weight = timeBlocks
-                        .subList(startIdx, endIdx - 1)
-                        .stream()
-                        .map(TimeBlockDto::weight)
-                        .reduce(0, Integer::sum);
-
-                response.add(new BestMeetingTimeVo(date, startTimeBlock.timeSlot(), endTimeBlock.timeSlot(), weight));
+            if (isSatisfiedDuration(startTimeBlock, endTimeBlock, duration)) {
+                int weight = sumTimeBlocksWeight(timeBlocks, startIdx, endIdx - 1);
+                response.add(new BestMeetingTimeVo(startTimeBlock.availableDate(), startTimeBlock.timeSlot(),
+                        endTimeBlock.timeSlot(), weight));
             }
         }
         return response;
+    }
+
+    private boolean isSatisfiedDuration(
+            TimeBlockDto startTimeBlock,
+            TimeBlockDto endTimeBlock,
+            Duration duration
+    ) {
+        int blockCnt = endTimeBlock.timeSlot().ordinal() - startTimeBlock.timeSlot().ordinal();
+        return blockCnt >= duration.getNeedBlock();
+    }
+
+    private int sumTimeBlocksWeight(
+            final List<TimeBlockDto> timeBlocks,
+            final int startIdx,
+            final int endIdx
+    ) {
+        return timeBlocks
+                .subList(startIdx, endIdx)
+                .stream()
+                .map(TimeBlockDto::weight)
+                .reduce(0, Integer::sum);
     }
 }
