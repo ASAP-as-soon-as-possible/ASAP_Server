0714d27e9a0ebc40bda5f2ace74ebf20e27cd768
KWY <wodrk1001@gmail.com>
2024-07-31 23:22:22 +0900

    #291 [test] 테스트 코드 추가
    각 요일들을 구분할 수 있는지 확인하는 테스트 코드

--- src/test/java/com/asap/server/service/meeting/recommend/strategy/impl/ContinuousMeetingTimeStrategyImplTest.java
+++ src/test/java/com/asap/server/service/meeting/recommend/strategy/impl/ContinuousMeetingTimeStrategyImplTest.java
@@ -2,7 +2,11 @@ package com.asap.server.service.meeting.recommend.strategy.impl;
 
 import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_12_00;
 import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_12_30;
+import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_13_00;
+import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_14_00;
+import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_14_30;
 import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_16_00;
+import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_17_00;
 import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_18_00;
 import static com.asap.server.persistence.domain.enums.TimeSlot.SLOT_20_00;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -222,6 +226,200 @@ class ContinuousMeetingTimeStrategyImplTest {
 
             // then
             assertThat(response.size()).isZero();
+        }
+    }
+
+    @DisplayName("timeBlocks 내에 2023-7-10일에는 12시부터 14시, 2023-7-11일에는 12시부터 13시까지 30분 간격으로 있고")
+    @Nested
+    class ReturnBestMeetingTimeVoByDuration2 {
+        // given
+        LocalDate availableDate = LocalDate.of(2023, 7, 10);
+        LocalDate availableDate2 = LocalDate.of(2023, 7, 11);
+        List<TimeBlockDto> tempTimeBlocks = TimeBlockDtoGenerator.generator(availableDate, SLOT_12_00, SLOT_14_00, 0,
+                2L);
+        List<TimeBlockDto> tempTimeBlocks2 = TimeBlockDtoGenerator.generator(availableDate2, SLOT_12_00, SLOT_13_00, 0,
+                2L);
+        List<TimeBlockDto> timeBlocks = new ArrayList<>() {{
+            addAll(tempTimeBlocks);
+            addAll(tempTimeBlocks2);
+        }};
+
+        @DisplayName("회의 진행 시간이 30분일 때, 12시부터 14시, 12시부터 13시까지인 BestMeetingTimeVo를 반환한다.")
+        @Test
+        void durationHalfTest() {
+            // given
+            BestMeetingTimeVo r1 = new BestMeetingTimeVo(availableDate, SLOT_12_00, SLOT_14_00, 0);
+            BestMeetingTimeVo r2 = new BestMeetingTimeVo(availableDate2, SLOT_12_00, SLOT_13_00, 0);
+            List<BestMeetingTimeVo> result = List.of(r1, r2);
+
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.HALF);
+
+            // then
+            assertThat(response).isEqualTo(result);
+        }
+
+        @DisplayName("회의 진행 시간이 1시간일 때, 12시부터 14시, 12시부터 13시까지인 BestMeetingTimeVo를 반환한다.")
+        @Test
+        void durationHourTest() {
+            // given
+            BestMeetingTimeVo r1 = new BestMeetingTimeVo(availableDate, SLOT_12_00, SLOT_14_00, 0);
+            BestMeetingTimeVo r2 = new BestMeetingTimeVo(availableDate2, SLOT_12_00, SLOT_13_00, 0);
+            List<BestMeetingTimeVo> result = List.of(r1, r2);
+
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.HOUR);
+
+            // then
+            assertThat(response).isEqualTo(result);
+        }
+
+        @DisplayName("회의 진행 시간이 1시간 30분일 때, 12시부터 14시까지인 BestMeetingTimeVo를 반환한다.")
+        @Test
+        void durationHourHalfTest() {
+            // given
+            BestMeetingTimeVo r1 = new BestMeetingTimeVo(availableDate, SLOT_12_00, SLOT_14_00, 0);
+            List<BestMeetingTimeVo> result = List.of(r1);
+
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.HOUR_HALF);
+
+            // then
+            assertThat(response).isEqualTo(result);
+        }
+
+        @DisplayName("회의 진행 시간이 2시간일 때, 12시부터 14시까지인 BestMeetingTimeVo를 반환한다.")
+        @Test
+        void durationTwoHourTest() {
+            // given
+            BestMeetingTimeVo r1 = new BestMeetingTimeVo(availableDate, SLOT_12_00, SLOT_14_00, 0);
+            List<BestMeetingTimeVo> result = List.of(r1);
+
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.TWO_HOUR);
+
+            // then
+            assertThat(response).isEqualTo(result);
+        }
+
+        @DisplayName("회의 진행 시간이 2시간 30분일 때, 빈 리스트를 반환한다.")
+        @Test
+        void durationTwoHourHalfTest() {
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.TWO_HOUR_HALF);
+
+            // then
+            assertThat(response.isEmpty()).isTrue();
+        }
+
+        @DisplayName("회의 진행 시간이 3시간일 때, 빈 리스트를 반환한다.")
+        @Test
+        void durationThreeHourTest() {
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.THREE_HOUR);
+
+            // then
+            assertThat(response.isEmpty()).isTrue();
+        }
+    }
+
+    @DisplayName("timeBlocks 내에 2023-7-10일에는 12시부터 14시, 2023-7-11일에는 14시30분부터 17시까지 30분 간격으로 있고")
+    @Nested
+    class ReturnBestMeetingTimeVoByDuration3 {
+        // given
+        LocalDate availableDate = LocalDate.of(2023, 7, 10);
+        LocalDate availableDate2 = LocalDate.of(2023, 7, 11);
+        List<TimeBlockDto> tempTimeBlocks = TimeBlockDtoGenerator.generator(availableDate, SLOT_12_00, SLOT_14_00, 0,
+                2L);
+        List<TimeBlockDto> tempTimeBlocks2 = TimeBlockDtoGenerator.generator(availableDate2, SLOT_14_30, SLOT_17_00, 0,
+                2L);
+        List<TimeBlockDto> timeBlocks = new ArrayList<>() {{
+            addAll(tempTimeBlocks);
+            addAll(tempTimeBlocks2);
+        }};
+
+        @DisplayName("회의 진행 시간이 30분일 때, 2023-7-10일 12시부터 14시, 2023-7-11일 14시 30분부터 17시까지인 BestMeetingTimeVo를 반환한다.")
+        @Test
+        void durationHalfTest() {
+            // given
+            BestMeetingTimeVo r1 = new BestMeetingTimeVo(availableDate, SLOT_12_00, SLOT_14_00, 0);
+            BestMeetingTimeVo r2 = new BestMeetingTimeVo(availableDate2, SLOT_14_30, SLOT_17_00, 0);
+            List<BestMeetingTimeVo> result = List.of(r1, r2);
+
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.HALF);
+
+            // then
+            assertThat(response).isEqualTo(result);
+        }
+
+        @DisplayName("회의 진행 시간이 30분일 때, 2023-7-10일 12시부터 14시, 2023-7-11일 14시 30분부터 17시까지인 BestMeetingTimeVo를 반환한다.")
+        @Test
+        void durationHourTest() {
+            // given
+            BestMeetingTimeVo r1 = new BestMeetingTimeVo(availableDate, SLOT_12_00, SLOT_14_00, 0);
+            BestMeetingTimeVo r2 = new BestMeetingTimeVo(availableDate2, SLOT_14_30, SLOT_17_00, 0);
+            List<BestMeetingTimeVo> result = List.of(r1, r2);
+
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.HOUR);
+
+            // then
+            assertThat(response).isEqualTo(result);
+        }
+
+        @DisplayName("회의 진행 시간이 30분일 때, 2023-7-10일 12시부터 14시, 2023-7-11일 14시 30분부터 17시까지인 BestMeetingTimeVo를 반환한다.")
+        @Test
+        void durationHourHalfTest() {
+            // given
+            BestMeetingTimeVo r1 = new BestMeetingTimeVo(availableDate, SLOT_12_00, SLOT_14_00, 0);
+            BestMeetingTimeVo r2 = new BestMeetingTimeVo(availableDate2, SLOT_14_30, SLOT_17_00, 0);
+            List<BestMeetingTimeVo> result = List.of(r1, r2);
+
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.HOUR_HALF);
+
+            // then
+            assertThat(response).isEqualTo(result);
+        }
+
+        @DisplayName("회의 진행 시간이 30분일 때, 2023-7-10일 12시부터 14시, 2023-7-11일 14시 30분부터 17시까지인 BestMeetingTimeVo를 반환한다.")
+        @Test
+        void durationTwoHourTest() {
+            // given
+            BestMeetingTimeVo r1 = new BestMeetingTimeVo(availableDate, SLOT_12_00, SLOT_14_00, 0);
+            BestMeetingTimeVo r2 = new BestMeetingTimeVo(availableDate2, SLOT_14_30, SLOT_17_00, 0);
+            List<BestMeetingTimeVo> result = List.of(r1, r2);
+
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.TWO_HOUR);
+
+            // then
+            assertThat(response).isEqualTo(result);
+        }
+
+        @DisplayName("회의 진행 시간이 30분일 때, 2023-7-11일 14시 30분부터 17시까지인 BestMeetingTimeVo를 반환한다.")
+        @Test
+        void durationTwoHourHalfTest() {
+            // given
+            BestMeetingTimeVo r1 = new BestMeetingTimeVo(availableDate2, SLOT_14_30, SLOT_17_00, 0);
+            List<BestMeetingTimeVo> result = List.of(r1);
+
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.TWO_HOUR_HALF);
+
+            // then
+            assertThat(response).isEqualTo(result);
+        }
+
+        @DisplayName("회의 진행 시간이 3시간일 때, 빈 리스트를 반환한다.")
+        @Test
+        void durationThreeHourTest() {
+            // when
+            List<BestMeetingTimeVo> response = continuousMeetingTimeStrategy.find(timeBlocks, Duration.THREE_HOUR);
+
+            // then
+            assertThat(response.isEmpty()).isTrue();
         }
     }
 }
