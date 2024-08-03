338f73011145eaf976d35beebb9e12164cd5d4d9
sohyundoh <lyny.doh@gmail.com>
2024-08-03 20:22:08 +0900

    #295 [feat] MeetingTimeCasesStratagy 구현

--- src/main/java/com/asap/server/service/meeting/recommend/strategy/MeetingTimeCasesStrategy.java
+++ src/main/java/com/asap/server/service/meeting/recommend/strategy/MeetingTimeCasesStrategy.java
@@ -4,6 +4,7 @@ import com.asap.server.persistence.domain.enums.Duration;
 import com.asap.server.service.vo.PossibleTimeCaseVo;
 import java.util.List;
 
-public interface MeetingTimeTimeCasesStrategy {
+public interface MeetingTimeCasesStrategy {
     List<PossibleTimeCaseVo> find(final Duration duration, int userCount);
+
 }
---
+++ src/main/java/com/asap/server/service/meeting/recommend/strategy/impl/MeetingTimeCasesStrategyImpl.java
@@ -0,0 +1,104 @@
+package com.asap.server.service.meeting.recommend.strategy.impl;
+
+import com.asap.server.persistence.domain.enums.Duration;
+import com.asap.server.service.meeting.recommend.strategy.MeetingTimeCasesStrategy;
+import com.asap.server.service.vo.PossibleTimeCaseVo;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.asap.server.persistence.domain.enums.Duration.HOUR;
+import static com.asap.server.persistence.domain.enums.Duration.HOUR_HALF;
+import static com.asap.server.persistence.domain.enums.Duration.TWO_HOUR;
+
+@Component
+public class MeetingTimeCasesStrategyImpl implements MeetingTimeCasesStrategy {
+    private static final Duration[] durations = Duration.values();
+
+    @Override
+    public List<PossibleTimeCaseVo> find(final Duration duration, int userCount) {
+        List<PossibleTimeCaseVo> possibleTimeCases = new ArrayList<>();
+        final int standard = (int) (userCount * 0.2);
+
+        if (duration.getNeedBlock() > TWO_HOUR.getNeedBlock()) {
+            possibleTimeCases.addAll(getPossibleTimeCasesOverTwoHourOverStandardRatio(duration, userCount, standard));
+            userCount -= standard + 1;
+
+            while (userCount >= 2) {
+                possibleTimeCases.addAll(findPossibleTimeCasesOverTwoHourUnderStandardRatio(duration, userCount, standard));
+                userCount -= standard + 1;
+            }
+
+        } else {
+
+            do {
+                possibleTimeCases.addAll(findPossibleTimeUnderTwoHour(duration, userCount, standard));
+                userCount -= standard + 1;
+            } while (userCount >= 2);
+
+        }
+        return possibleTimeCases;
+    }
+
+    public List<PossibleTimeCaseVo> getPossibleTimeCasesOverTwoHourOverStandardRatio(final Duration duration, final int userCount, final int standard) {
+        List<PossibleTimeCaseVo> possibleTimeCases = new ArrayList<>(findPossibleTimeCasesUnit(duration, userCount, standard));
+
+        Duration nextDuration = duration;
+
+        while (nextDuration.getNeedBlock() > HOUR.getNeedBlock() && nextDuration.getNeedBlock() >= duration.getNeedBlock() - TWO_HOUR.getNeedBlock()) {
+            nextDuration = durations[nextDuration.getNeedBlock() - 3];
+            possibleTimeCases.addAll(findPossibleTimeCasesUnit(nextDuration, userCount, standard));
+        }
+
+        return possibleTimeCases;
+    }
+
+    public List<PossibleTimeCaseVo> findPossibleTimeCasesOverTwoHourUnderStandardRatio(final Duration duration, final int userCount, final int standard) {
+        List<PossibleTimeCaseVo> possibleTimeCases = new ArrayList<>();
+        Duration nextDuration = duration;
+
+        while (nextDuration.getNeedBlock() > duration.getNeedBlock() - HOUR.getNeedBlock() && nextDuration.getNeedBlock() > HOUR.getNeedBlock()) {
+            possibleTimeCases.addAll(findPossibleTimeCasesUnit(nextDuration, userCount, standard));
+            nextDuration = durations[nextDuration.getNeedBlock() - 3];
+        }
+
+        for (int count = userCount; count >= userCount - standard; count--) {
+            possibleTimeCases.add(new PossibleTimeCaseVo(nextDuration, count));
+        }
+
+        return possibleTimeCases;
+    }
+
+
+    public List<PossibleTimeCaseVo> findPossibleTimeUnderTwoHour(final Duration duration, final int userCount, final int standard) {
+        List<PossibleTimeCaseVo> possibleTimeCases = new ArrayList<>(findPossibleTimeCasesUnit(duration, userCount, standard));
+
+        Duration nextDuration = duration;
+
+        while (nextDuration.getNeedBlock() > HOUR.getNeedBlock()) {
+            nextDuration = durations[nextDuration.getNeedBlock() - 3];
+            possibleTimeCases.addAll(findPossibleTimeCasesUnit(nextDuration, userCount, standard));
+        }
+
+        if (duration == HOUR) {
+            possibleTimeCases.add(new PossibleTimeCaseVo(durations[duration.getNeedBlock() - 2], userCount));
+        }
+
+        return possibleTimeCases;
+    }
+
+    public List<PossibleTimeCaseVo> findPossibleTimeCasesUnit(final Duration duration, final int userCount, final int standard) {
+        List<PossibleTimeCaseVo> possibleTimeCases = new ArrayList<>();
+
+        for (int count = userCount; count >= userCount - standard; count--) {
+            possibleTimeCases.add(new PossibleTimeCaseVo(duration, count));
+
+            if (duration.getNeedBlock() < HOUR_HALF.getNeedBlock()) continue;
+
+            possibleTimeCases.add(new PossibleTimeCaseVo(durations[duration.getNeedBlock() - 2], count));
+
+        }
+        return possibleTimeCases;
+    }
+}
