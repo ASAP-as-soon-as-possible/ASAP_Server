94861fb460bed0afb9e2496135c5465c4b250c72
KWY <wodrk1001@gmail.com>
2024-08-14 16:19:38 +0900

    #309 [feat] 사용자 이름 값 객체 구현

---
+++ src/main/java/com/asap/server/persistence/domain/user/Name.java
@@ -0,0 +1,28 @@
+package com.asap.server.persistence.domain.user;
+
+import static com.asap.server.common.exception.Error.USERNAME_NOT_BLANK_EXCEPTION;
+import static com.asap.server.common.exception.Error.USERNAME_NOT_NULL_EXCEPTION;
+import static com.asap.server.common.exception.Error.USERNAME_TOO_LONG_EXCEPTION;
+
+import com.asap.server.common.exception.model.BadRequestException;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record Name(String value) {
+    private static final int MAX_NAME_LENGTH = 8;
+
+    public Name(String value) {
+        if (value == null) {
+            throw new BadRequestException(USERNAME_NOT_NULL_EXCEPTION);
+        }
+        if (value.isBlank()) {
+            throw new BadRequestException(USERNAME_NOT_BLANK_EXCEPTION);
+        }
+        if (value.trim().length() > MAX_NAME_LENGTH) {
+            throw new BadRequestException(USERNAME_TOO_LONG_EXCEPTION);
+        }
+
+        this.value = value.trim();
+    }
+}
+
