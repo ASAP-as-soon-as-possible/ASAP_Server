59fad3224673c4b21e0c13cc119a24b659bf0d70
KWY <wodrk1001@gmail.com>
2024-08-21 00:42:04 +0900

    #318 [fix] 매개변수 객체 타입에서 원시 타입으로 수정

--- src/main/java/com/asap/server/persistence/repository/UserMeetingScheduleRepository.java
+++ src/main/java/com/asap/server/persistence/repository/UserMeetingScheduleRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.repository.Repository;
 public interface UserMeetingScheduleRepository extends Repository<UserMeetingSchedule, Long> {
     void save(final UserMeetingSchedule userMeetingSchedule);
 
-    List<UserMeetingSchedule> findAllByMeetingId(final Long meetingId);
+    List<UserMeetingSchedule> findAllByMeetingId(final long meetingId);
 }
