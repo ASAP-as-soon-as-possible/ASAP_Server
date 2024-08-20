e876e1e04771d57805a8a28f31acf8191d5cae53
KWY <wodrk1001@gmail.com>
2024-08-20 20:47:22 +0900

    #318 [feat] 회의 시간 대 별 유저를 구하는 비즈니스 로직 구현

--- src/main/java/com/asap/server/service/time/UserMeetingScheduleService.java
+++ src/main/java/com/asap/server/service/time/UserMeetingScheduleService.java
@@ -1,9 +1,17 @@
 package com.asap.server.service.time;
 
 import com.asap.server.common.utils.DateUtil;
+import com.asap.server.persistence.domain.enums.TimeSlot;
 import com.asap.server.persistence.domain.time.UserMeetingSchedule;
 import com.asap.server.persistence.repository.UserMeetingScheduleRepository;
 import com.asap.server.service.time.dto.UserMeetingScheduleRegisterDto;
+import com.asap.server.service.time.vo.TimeBlock;
+import com.asap.server.service.time.vo.UserScheduleByTimeSlot;
+import com.asap.server.service.time.vo.UserScheduleByTimeSlot.CompositeKey;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,7 +22,11 @@ public class UserMeetingScheduleService {
     private final UserMeetingScheduleRepository userMeetingScheduleRepository;
 
     @Transactional
-    public void createUserMeetingSchedule(final Long userId, final Long meetingId, final UserMeetingScheduleRegisterDto registerDto) {
+    public void createUserMeetingSchedule(
+            final Long userId,
+            final Long meetingId,
+            final UserMeetingScheduleRegisterDto registerDto
+    ) {
         UserMeetingSchedule userMeetingSchedule = UserMeetingSchedule.builder()
                 .userId(userId)
                 .meetingId(meetingId)
@@ -23,5 +35,50 @@ public class UserMeetingScheduleService {
                 .endTimeSlot(registerDto.endTime())
                 .build();
         userMeetingScheduleRepository.save(userMeetingSchedule);
+    }
+
+    @Transactional(readOnly = true)
+    public List<TimeBlock> getTimeBlocks(final Long meetingId) {
+        List<UserMeetingSchedule> userMeetingSchedules = userMeetingScheduleRepository.findAllByMeetingId(meetingId);
+
+        return userMeetingSchedules.stream()
+                .flatMap(this::convertToUserScheduleByTimeSlot)
+                .collect(Collectors.groupingBy(UserScheduleByTimeSlot::composeKey))
+                .entrySet().stream()
+                .map(this::convertToTimeBlock)
+                .sorted()
+                .toList();
+    }
+
+    private Stream<UserScheduleByTimeSlot> convertToUserScheduleByTimeSlot(
+            final UserMeetingSchedule userMeetingSchedule
+    ) {
+        return TimeSlot.getTimeSlots(
+                        userMeetingSchedule.getStartTimeSlot().getIndex(),
+                        userMeetingSchedule.getEndTimeSlot().getIndex()
+                )
+                .stream()
+                .map(timeSlot -> new UserScheduleByTimeSlot(
+                                userMeetingSchedule.getId(),
+                                userMeetingSchedule.getAvailableDate(),
+                                userMeetingSchedule.getUserId(),
+                                timeSlot,
+                                userMeetingSchedule.getWeight()
+                        )
+                );
+    }
+
+    private TimeBlock convertToTimeBlock(
+            final Entry<CompositeKey, List<UserScheduleByTimeSlot>> entry
+    ) {
+        List<Long> users = entry.getValue().stream()
+                .map(UserScheduleByTimeSlot::userId)
+                .toList();
+
+        int weight = entry.getValue().stream()
+                .mapToInt(UserScheduleByTimeSlot::weight)
+                .sum();
+
+        return new TimeBlock(entry.getKey().availableDate(), entry.getKey().time(), weight, users);
     }
 }
