b2ac703a50bd9431beed380f35b18c52e23f19c7
KWY <wodrk1001@gmail.com>
2024-08-20 20:31:49 +0900

    #318 [feat] 사용자 별 회의 시간대 조회 jpa 구현

--- src/main/java/com/asap/server/persistence/repository/UserMeetingScheduleRepository.java
+++ src/main/java/com/asap/server/persistence/repository/UserMeetingScheduleRepository.java
@@ -1,8 +1,11 @@
 package com.asap.server.persistence.repository;
 
 import com.asap.server.persistence.domain.time.UserMeetingSchedule;
+import java.util.List;
 import org.springframework.data.repository.Repository;
 
 public interface UserMeetingScheduleRepository extends Repository<UserMeetingSchedule, Long> {
     void save(final UserMeetingSchedule userMeetingSchedule);
+
+    List<UserMeetingSchedule> findAllByMeetingId(final Long meetingId);
 }
