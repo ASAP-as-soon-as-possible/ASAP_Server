617c66da1efee54292f0ab44bb94b6937d01564e
KWY <wodrk1001@gmail.com>
2025-01-19 16:13:30 +0900

    #338 [feat] 타 스레드에 mdc 문맥 전파

--- src/main/java/com/asap/server/presentation/config/async/AsyncConfig.java
+++ src/main/java/com/asap/server/presentation/config/async/AsyncConfig.java
@@ -3,6 +3,7 @@ package com.asap.server.presentation.config.async;
 import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -10,11 +11,12 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 public class AsyncConfig {
     @Bean
-    public Executor threadPoolTaskExecutor() {
+    public Executor threadPoolTaskExecutor(TaskDecorator taskDecorator) {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         taskExecutor.setCorePoolSize(1);
         taskExecutor.setQueueCapacity(10);
         taskExecutor.setMaxPoolSize(3);
+        taskExecutor.setTaskDecorator(taskDecorator);
         return taskExecutor;
     }
 }
